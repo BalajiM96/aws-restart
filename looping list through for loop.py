55b7898ca6e5e9fe0c95ac77b4fdc7b6f3fceceb
BalajiM96 <119749991+BalajiM96@users.noreply.github.com>
2023-03-31 13:12:30 +0530

    Add files via upload

---
+++ looping list through for loop.py
@@ -0,0 +1,4 @@
+myMixedList = [45, 290578, 1.02, True, "My dog is on the bed.", "45"]
+
+for i in myMixedList:
+    print("{} is of datatype {}".format(i, type(i)))
---
+++ string.py
@@ -0,0 +1,10 @@
+myVar= "This is my string"
+print(myVar)
+print(type(myVar))
+print("Type of the" + myVar +" is {}".format(str(type(myVar))))
+
+a = "xxx"
+b = "yyy"
+c = "zzz"
+
+print(a + b + c)
